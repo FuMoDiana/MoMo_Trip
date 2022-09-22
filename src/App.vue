56417ffdeb415223205fb7254a9668894bbbaadd
cxz <2405819294@qq.com>
2022-09-22 22:13:40 +0800

    搭建项目基础结构

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="app">
       App vue
+      <router-view></router-view>
     </div>
 </template>
 
---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    //映射关系：route -> component
+    routes: [{
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            component: () =>
+                import ("../views/home/home.vue")
+
+        },
+        {
+            path: '/message',
+            component: () =>
+                import ("../views/message/message.vue")
+        },
+        {
+            path: '/favor',
+            component: () =>
+                import ("../views/favor/favor.vue")
+        },
+        {
+            path: '/order',
+            component: () =>
+                import ("../views/order/order.vue")
+        }
+
+    ]
+})
+
+export default router
